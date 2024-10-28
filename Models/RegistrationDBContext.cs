e10696a2bdf0a4081326d9f861d9c3d44a12dbd5
Oliver Vincent <oliverlund3@gmail.com>
2024-10-28 11:11:36 +0100

    Commit

---
+++ Models/RegistrationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTeaching_RazorPages.Models
+{
+    public class RegistrationDBContext:DbContext
+    {
+         protected override void OnConfiguring(DbContextOptionsBuilder  options)
+        {
+            options.UseSqlServer (@"Server=localhost,1433;Database=master;User Id=sa;Password=Localhost1!;TrustServerCertificate=True;");
+        }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Enrollment> Enrollments{ get; set; }
+    }
+}
